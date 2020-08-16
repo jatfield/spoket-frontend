f73ab9fa6a86420a7b6d193807efb98a8d5f3cc7
Pálóczi Ádám <palocziad@gmail.com>
2020-08-16 21:33:24 +0200

    include completers in participants

--- src/trips/components/TripParticipants.js
+++ src/trips/components/TripParticipants.js
@@ -46,6 +46,10 @@ const TripParticipants = (props) => {
       {isLoading && <LoadingSpinner />}
       {!isLoading && riders && <div className="trip_riders">
         <div className="trip_riders__participants">
+          <h4>Teljesítők:</h4>
+          {riders.completers.map((p) => <Participant participant = {p} key = {p._id} trip = {props.trip} role = 'participant' user = {props.user} /> )}
+        </div>
+        <div className="trip_riders__participants">
           <h4>Résztvevők:</h4>
           {riders.participants.map((p) => <Participant participant = {p} key = {p._id} trip = {props.trip} role = 'participant' user = {props.user} /> )}
         </div>
